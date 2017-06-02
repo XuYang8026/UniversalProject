e426521bdfd6f81fca51bf29cc32264da8c01b8f
徐阳 <xuyang@yangs-macbook-pro.local>
2017-06-02 20:09:03 +0800

    略微调整

---
+++ MiAiApp/Base/Button/XYButton.h
@@ -0,0 +1,13 @@
+//
+//  XYButton.h
+//  MiAiApp
+//
+//  Created by 徐阳 on 2017/6/2.
+//  Copyright © 2017年 徐阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYButton : UIButton
+
+@end
---
+++ MiAiApp/Base/Button/XYButton.m
@@ -0,0 +1,15 @@
+//
+//  XYButton.m
+//  MiAiApp
+//
+//  Created by 徐阳 on 2017/6/2.
+//  Copyright © 2017年 徐阳. All rights reserved.
+//
+
+#import "XYButton.h"
+
+@implementation XYButton
+
+
+
+@end
